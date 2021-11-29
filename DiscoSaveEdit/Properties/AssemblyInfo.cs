d7c65925fe25e5cbd9ddd7f181e7e52b50f1c0f5
Justin Bloomfield <justin@jbloomfield.site>
2021-11-28 23:49:28 -0600

    Fixed AsseblyInfo again

--- DiscoSaveEdit/Properties/AssemblyInfo.cs
+++ DiscoSaveEdit/Properties/AssemblyInfo.cs
@@ -2,35 +2,30 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DiscoSaveEdit")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Save Editor for Disco Elysium")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("jbloomfield.site")]
 [assembly: AssemblyProduct("DiscoSaveEdit")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e5f7343-a1a0-4c25-ab15-7e382739f568")]
+[assembly: Guid("b688308f-bec7-43c6-bfb2-1cedf9805203")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
