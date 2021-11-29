e26f20ca092269ef769878e491be7866757f8855
Justin Bloomfield <justin@jbloomfield.site>
2021-11-28 23:45:34 -0600

    Added AssemblyInfo.cs

---
+++ DiscoSaveEdit/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DiscoSaveEdit")]
+[assembly: AssemblyDescription("Save Editor for Disco Elysium")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("jbloomfield.site")]
+[assembly: AssemblyProduct("DiscoSaveEdit")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b688308f-bec7-43c6-bfb2-1cedf9805203")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
